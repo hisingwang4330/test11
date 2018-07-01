dd3e051f4bce8cb527182a3b6f100425e963f749
hisingwang4330 <>
2018-07-01 15:46:49 +0700

    11

---
+++ ddd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ddd
+//
+//  Created by JmoVxia on 2018/7/1.
+//  Copyright © 2018年 JmoVxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ddd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ddd
+//
+//  Created by JmoVxia on 2018/7/1.
+//  Copyright © 2018年 JmoVxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
