6e76f0460c59425b0b11c1fc13ae77c849fee576
hising123 <hisingwang@126.com>
2018-08-17 15:34:59 +0700

    dd

---
+++ ddd/ddViewController.h
@@ -0,0 +1,13 @@
+//
+//  ddViewController.h
+//  ddd
+//
+//  Created by JmoVxia on 2018/8/17.
+//  Copyright © 2018年 JmoVxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ddViewController : UIViewController
+
+@end
---
+++ ddd/ddViewController.m
@@ -0,0 +1,43 @@
+//
+//  ddViewController.m
+//  ddd
+//
+//  Created by JmoVxia on 2018/8/17.
+//  Copyright © 2018年 JmoVxia. All rights reserved.
+//
+
+#import "ddViewController.h"
+
+@interface ddViewController ()
+
+@end
+
+@implementation ddViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"hahah");
+
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
